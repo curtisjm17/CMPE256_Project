e3c810bb52d094c4769d3be5da108589c53dfa71
Ben <benhochstadt@gmail.com>
2022-03-10 03:37:43 -0500

    Automated recommended playlists!

--- spotipy_api.py
+++ spotipy_api.py
@@ -1,7 +1,7 @@
 #!/bin/python
 
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 import pickle
 import time
 import random
@@ -9,19 +9,27 @@ import string
 from datetime import datetime as dt
 import pandas as pd
 import numpy as np
-import csv
 
 # Million Playlist Dataset contains playlists created between January 2010 and November 2017
 class EasySpotipy():
-    __ID           = 'fb03f1c6af28420db3441d201a8dea16'
-    __SECRET       = '543f352d8d694958abfcfde732a474ac'
     __TRACK_ID_MAP = 'data/tracks.csv'
 
-    def __init__(self, id=__ID, secret=__SECRET, track_id_map=__TRACK_ID_MAP, max_retries=10):
-        creds    = SpotifyClientCredentials(client_id=id, client_secret=secret)
-        self.sp_ = spotipy.Spotify(client_credentials_manager=creds, retries=max_retries)
+    def __init__(self, username=None, track_id_map=__TRACK_ID_MAP, max_retries=10, cred_file='data/spotipy.txt'):
+        with open(cred_file) as f:
+            id = f.readline()[:-1]
+            secret = f.readline()
+
+        token  = spotipy.util.prompt_for_user_token(
+                 username=username,
+                 scope='playlist-modify-public',
+                 client_id=id,
+                 client_secret=secret,
+                 redirect_uri="http://localhost:8888")
+        self.sp_ = spotipy.Spotify(auth=token)
+
         # Track ID Map
         self.track_id_map_ = pd.read_csv(track_id_map, header=None, names=['track_index', 'track_id'])
+        self.username = username
 
     def getSP(self):
         return self.sp_
@@ -104,7 +112,10 @@ class EasySpotipy():
                     for song in results['items']:
                         if not song['track']:
                             continue
-                        release_date = song['track']['album'].get('release_date')
+                        try:
+                            release_date = song['track']['album'].get('release_date')
+                        except:
+                            continue
 
                         if release_date and release_date < latest_release_date:
                             track_id = song['track']['id']
@@ -122,49 +133,32 @@ class EasySpotipy():
                         break
         return np.array(tracklist)
 
-    def addTracksToPlaylist(self, tracks):
-        return
-
-
-    # def getRecommendations(self, seed_artists):
-    #     if len(seed_artists > 5):
-    #         return []
-
-    # def isGenre(self, track, genre):
-    #     return False
-
-    # def EvaluateGenres(self, playlist, genre):
-    #     return 0
-
-    # def PerformEvaluationMetrics(self, provided_playlists, recommended_playlists, num_trimmed, num_produced):
-    #     '''
-    #     '''
-    #     return 0
+    def createNewPlaylist(self, tracks, playlist_name, user='Hochstadt'):
+        playlist_id = self.sp_.user_playlist_create(user, playlist_name)
+        self.sp_.playlist_add_items(playlist_id['id'], tracks)
+        return playlist_id
 
 def main():
-    sp_api = EasySpotipy()
-    NUM_PLAYLISTS=10
-    MIN_TRACKS_PER_PLAYLIST=10
-    MAX_TRACKS_PER_PLAYLIST=10
+    sp_api = EasySpotipy(username='Hochstadt')
 
-    start_time = time.time()
-    for i in range(NUM_PLAYLISTS):
-        playlist_time = time.time()
-        random_playlist = sp_api.getRandomPlaylist(min_tracks=MIN_TRACKS_PER_PLAYLIST, max_tracks=MAX_TRACKS_PER_PLAYLIST)
-    #     # Col0 is Track ID
-    #     # Col1 is Track index (in map)
-    #     # Col2 is Track name
-        print(f'Random playlist #{i} took {time.time() - playlist_time}s:\n{random_playlist[:,2]}')
-    print(f'It took {time.time() - start_time}s to pull {NUM_PLAYLISTS} compatible playlists!')
+    # sp_api.getSP().
+    # SP = sp_api.getSP()
+    # print(SP.me())
+    # ID     = '8ec413d8eed346bd9f88fab92cee1dfd'
+    # SECRET = '1bc60fc14b0347fa971eee940e7ecd24'
+    # creds  = SpotifyClientCredentials(client_id=ID, client_secret=SECRET)
+    # oauth  = SpotifyOAuth(client_id=ID, client_secret=SECRET, redirect_uri='localhost:8080')
+    # sp     = spotipy.Spotify(client_credentials_manager=creds, auth_manager=oauth)
 
-    # sp_api.addTracksToPlaylist('abc')
-    print(sp_api.getTrackIDFromIndex(1))
-    # sp_api.getTrackIDFromIndex(1)
-    # trkmap = sp_api.track_id_map_
-    # track_id = trkmap[trkmap.track_index == 1].track_id
-    # track_index = trkmap[trkmap.track_id == '6I9VzXrHxO9rA9A5euc8Ak'].track_index.values[0]
-    # print(track_id)
 
+    # playlist_ID = sp.user_playlist_create('Hochstadt', 'Potato Salad')
+
+
+
+    # Get random playlist
+    # Put into playlist
+    # insert into LSH model
+    # Add new playlist
 
 if __name__ == "__main__":
     main()
